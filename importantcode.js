562ba23d6f6581ee795701f10771b72f171fed4c
soni kumari <67049547+SoniKumari30@users.noreply.github.com>
2021-08-09 20:41:35 +0530

    first commit

--- importantcode.js
+++ importantcode.js
@@ -134,20 +134,20 @@
 // Fibonacci Series
 
 
-// let num = 4
-// function fibonacci(number){
-//   let n1 = 0, n2 = 1, nextTerm;
+let num = 4
+function fibonacci(number){
+  let n1 = 0, n2 = 1, nextTerm;
 
-//   console.log('Fibonacci Series:');
+  console.log('Fibonacci Series:');
   
-//   for (let i = 1; i <= number; i++) {
-//       console.log(n1);
-//       nextTerm = n1 + n2;
-//       n1 = n2;
-//       n2 = nextTerm;
-//   }
-// }
-// console.log(fibonacci(num))
+  for (let i = 1; i <= number; i++) {
+      console.log(n1);
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+  }
+}
+console.log(fibonacci(num))
 
 
 // Find a max no in an array.
